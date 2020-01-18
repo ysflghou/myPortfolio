bce6b5e0650fcf302089cd5d6d594b125ed29984
lghou <yousseflghoumaigui@gmail.com>
2020-01-18 19:05:21 +0100

    remove home component lazy load

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Home from "@/views/Home.vue";
 
 Vue.use(VueRouter);
 
@@ -22,7 +23,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (home.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "home" */ "../views/Home.vue")
+    component: Home
   },
   {
     path: "*",
